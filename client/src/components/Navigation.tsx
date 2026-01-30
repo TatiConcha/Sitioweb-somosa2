aed596d6677c4a070e392b01274610b1540e6b14
Tatiana Concha <tatianaconcha.v@gmail.com>
2026-01-30 17:11:09 -0300

    Fix case-sensitive logo filename

--- client/src/components/Navigation.tsx
+++ client/src/components/Navigation.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { Menu, X, MessageCircle } from 'lucide-react';
 import { Button } from '@/components/ui/button';
-import logo from '../assets/logo.png';
+import logo from '../assets/Logo.png';
 
 
 /**
