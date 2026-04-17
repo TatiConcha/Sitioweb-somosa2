f9eb6130ab0363b231f5fba38e5d7cc43dc46787
Tatiana Concha <tatianaconcha.v@gmail.com>
2026-04-17 16:30:54 -0400

    ajustes de redes sociales

--- client/src/components/SocialMediaSection.tsx
+++ client/src/components/SocialMediaSection.tsx
@@ -15,18 +15,21 @@ const instagramPosts = [
     image: '/images/instagram-1.jpg',
     caption: 'Transformación exterior completa en Lo Barnechea',
     link: 'https://www.instagram.com/p/C_Jve20PsJM/?img_index=1',
+    isVideo: true
   },
   {
     id: '2',
     image: '/images/instagram-2.jpg',
     caption: 'Renovación interior moderna y elegante',
     link: 'https://www.instagram.com/p/C-puTRvO0ej/?img_index=1',
+    isVideo: false
   },
   {
     id: '3',
     image: '/images/instagram-3.jpg',
     caption: 'Oficinas corporativas de clase mundial',
     link: 'https://www.instagram.com/p/C48rlJ_RYaq/',
+    isVideo: true
   },
 ];
 
@@ -65,6 +68,22 @@ export default function SocialMediaSection() {
                 alt={post.caption}
                 className="w-full h-full object-cover transition-transform duration-500 group-hover:scale-110"
               />
+
+{post.isVideo && (
+  <div className="absolute inset-0 flex items-center justify-center pointer-events-none">
+    <div className="bg-black/50 rounded-full p-4">
+      <svg
+        width="28"
+        height="28"
+        viewBox="0 0 24 24"
+        fill="white"
+      >
+        <path d="M8 5v14l11-7z" />
+      </svg>
+    </div>
+  </div>
+)}
+
 
               {/* Overlay */}
               <div className="absolute inset-0 bg-gradient-to-t from-black/80 via-black/40 to-transparent opacity-0 group-hover:opacity-100 transition-opacity duration-300 flex flex-col justify-end p-4">
