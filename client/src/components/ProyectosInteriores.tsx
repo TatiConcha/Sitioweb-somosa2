d9d3e104d2ea43c706e2fc9ba67d89f5d2b64bc3
Tatiana Concha <tatianaconcha.v@gmail.com>
2026-05-15 14:19:08 -0400

    proyectos

--- client/src/components/ProyectosInteriores.tsx
+++ client/src/components/ProyectosInteriores.tsx
@@ -42,7 +42,7 @@ export default function ProyectosInteriores() {
 
           {/* GRID */}
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-            {projects.map((project) => (
+            {interiorProjects.map((project) => (
        
  <Link
     href={`/proyectos/${project.slug}`}
